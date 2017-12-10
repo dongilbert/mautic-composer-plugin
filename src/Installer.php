f177fbf0bdd0f0bf929660abc8940f6b287c6f98
Don Gilbert <don.gilbert@mautic.com>
2017-12-10 13:22:53 -0500

    Initial Commit

---
+++ src/Installer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mautic\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class Installer extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $packageType = $package->getType();
+        $packageName = $package->getPrettyName();
+
+        switch (true) {
+            case strpos($packageType, 'mautic-theme') === 0:
+                $this->vendorDir = 'themes/vendor';
+                return 'themes/'.$packageName;
+                break;
+            case strpos($packageType, 'mautic/plugin-') === 0:
+                $this->vendorDir = 'plugins/vendor';
+                return 'plugins/'.$packageName;
+                break;
+            default:
+                throw new \InvalidArgumentException(
+                    'Unable to install. '
+                );
+
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        $supportedTypes = [
+            'mautic-plugin',
+            'mautic-theme',
+        ];
+
+        return in_array($packageType, $supportedTypes);
+    }
+}
